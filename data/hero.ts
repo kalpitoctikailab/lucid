0fef9a8a766b53dcf4c5bfc5abdc2e6837a87cd7
kalpitoctikailab <kalpit.octikailab@gmail.com>
2026-05-02 01:52:49 +0530

    Update Herobanner IMage

--- data/hero.ts
+++ data/hero.ts
@@ -3,7 +3,8 @@
  * Place your own 4K property footage at public/videos/hero.mp4
  */
 
-export const HERO_VIDEO = "/videos/Lucid-Website-HeroBanner.mp4";
+export const HERO_VIDEO =
+  "https://api8t4ymugclvz0l.public.blob.vercel-storage.com/Lucid-Website-HeroBanner.mp4";
 
 export const HERO_VIDEO_FALLBACK =
   "https://videos.pexels.com/video-files/5349086/5349086-hd_1920_1080_30fps.mp4";
